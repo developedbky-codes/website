a1f35303b60e1d7bf0047dfe2314d0ef833646c4
developedbky-codes <k_andrews@developedbyk.com>
2026-09-05 01:20:07 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+<script>
+function openConsultationModal() {
+  document.getElementById("consultationModal").style.display = "flex";
+}
+
+function closeConsultationModal() {
+  document.getElementById("consultationModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById("consultationModal");
+
+  if (event.target === modal) {
+    closeConsultationModal();
+  }
+}
+</script>
